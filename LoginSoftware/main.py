4fcba8b565ce06e0236da80dc4c3c762b5db0cd7
孟凡钧 <1437615458@qq.com>
2024-04-12 19:55:12 +0800

    update window

---
+++ LoginSoftware/main.py
@@ -0,0 +1,25 @@
+from LoginUi import *
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_LoginWindow()
+        self.ui.setupUi(self)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.shadow = QtWidgets.QGraphicsDropShadowEffect(self)
+        self.shadow.setOffset(5,5)
+        self.shadow.setBlurRadius(15)
+        self.shadow.setColor(QtCore.Qt.gray)
+        self.ui.frame.setGraphicsEffect(self.shadow)
+        self.show()
+
+
+if __name__ == '__main__':
+    app= QApplication(sys.argv)
+    win = LoginWindow()
+    sys.exit(app.exec_())
+
